19dde0d19a9bb62c6b5b59deb3dd3b45808e0c83
Aurelien Bouron <aurelien.bouron@gmail.com>
2018-06-06 23:10:42 +0200

    feat: Open cards when we click on them

--- js/app.js
+++ js/app.js
@@ -36,3 +36,13 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+ const deckOfCards = document.querySelectorAll('.card');
+ const openCards = [];
+
+ deckOfCards.forEach(function(card) {
+     card.addEventListener('click',function(event) {
+         card.classList.add('open', 'show');
+     })
+ })
